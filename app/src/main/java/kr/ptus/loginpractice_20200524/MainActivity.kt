ade810364ad34dc001408705b13e5326c85676cc
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-24 12:12:09 +0900

    when을 활용해서 입력 id 검사

--- app/src/main/java/kr/ptus/loginpractice_20200524/MainActivity.kt
+++ app/src/main/java/kr/ptus/loginpractice_20200524/MainActivity.kt
@@ -13,11 +13,29 @@ class MainActivity : AppCompatActivity() {
 
         loginBtn.setOnClickListener {
 
-            val inputIp = idEdt.text.toString()
+            val inputId = idEdt.text.toString()
             val inputPw = pwEdt.text.toString()
 
-            Log.d("입력아이디", inputIp)
+            Log.d("입력아이디", inputId)
             Log.d("입력비번",inputPw)
+
+            /* if (inputId == "admin"){
+                Log.d("입력ID 확인", "admin임")
+            }
+            else if (inputId == "admin@admin.com"){
+                Log.d("입력ID 확인", "관리자 이메일")
+            }
+            else {
+                Log.d("입력ID 확인", "틀린아이디")
+            } */
+
+            when (inputId){
+                "admin" -> {Log.d("입력ID 확인", "admin임")}
+                "admin@admin.com" -> {Log.d("입력ID 확인", "관리자 이메일")}
+                else -> {Log.d("입력ID 확인", "틀린아이디")}
+            }
+
+
 
         }
 
