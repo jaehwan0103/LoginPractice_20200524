3d92a13223eb9a4d0dea019fa2e8a5cf867eb0df
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-24 11:26:07 +0900

    아이디와 비밀번호를 입력 받아 출력

--- app/src/main/java/kr/ptus/loginpractice_20200524/MainActivity.kt
+++ app/src/main/java/kr/ptus/loginpractice_20200524/MainActivity.kt
@@ -2,11 +2,24 @@ package kr.ptus.loginpractice_20200524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputIp = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            Log.d("입력아이디", inputIp)
+            Log.d("입력비번",inputPw)
+
+        }
+
     }
 }
